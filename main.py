5d4d723a05839d53301895c647ca565b3e2ba435
ZhMax1 <151944775+ZhMax1@users.noreply.github.com>
2024-02-27 17:06:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return render_template('choice.html', planet_name=planet_name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
